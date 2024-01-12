e955424e87777175b0c431fdb5ba819d7ff08957
Trevor Tran <trevor.phuong.tran@gmail.com>
2024-01-11 16:05:00 -0800

    temp

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-import Header from "./components/header/Header";
 import Home from "./components/home/Home";
-import Footer from "./components/footer/Footer";
-
+import SideNavbar from "./components/sidebar/SideNavbar";
 import './App.scss';
 
 function App() {
   return (
-    <div className="App container-fluid p-0">
-      <div className="row">
-        <div className="col">
-          <Home/>
+    <>
+      <div className="App container-fluid p-0">
+        <div className="row">
+          <div className="col">
+            <Home />
+          </div>
+        </div>
+
+        <div className="App container-fluid p-0">
+          <div className="row vw-100 vw-100">
+            <div className="col">
+              <SideNavbar />
+            </div>
+            <div className="col">
+              something
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <SideNavbar />
+          </div>
+          <div className="col">
+            something
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <SideNavbar />
+          </div>
+          <div className="col">
+            something
+          </div>
+          <div className="col-sm">
+            <p>footer</p>
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-sm">
-          <Header/>
-        </div>
-      </div>
-      <div className="row">
-        {/* <div className="col-sm">
-          <p>left</p>
-        </div> */}
-        <div className="col-sm">
-          <div className="mid"><p>mid</p></div>
-        </div>
-        {/* <div className="col-sm">
-          <p>right</p>
-        </div> */}
-      </div>
-      <div className="row">
-        <div className="col-sm">
-          <p>footer</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/sidebar/SideNavbar.js
@@ -0,0 +1,24 @@
+export default function SideNavbar() {
+
+  return (
+    <>
+      <div className="short-description">
+        <h1 className="full-name">Trevor Tran</h1>
+        <h4 className="job-title">Job title</h4>
+        <p className="job-title">Job title</p>
+      </div>
+
+      <div className="navigation d-flex flex-direction-column">
+        <div>
+          <a className="navbar-brand" href="#">Navbar</a>
+        </div>
+        <div>
+          <a className="navbar-brand" href="#">Navbar</a>
+        </div>
+        <div>
+          <a className="navbar-brand" href="#">Navbar</a>
+        </div>
+      </div>
+    </>
+  )
+}
