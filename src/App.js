54e2a85098320c8350881ac7fce4e7654e520de7
Trevor Tran <fuongdtran@gmail.com>
2024-01-13 15:47:39 -0800

    links responsive to focused section

--- src/App.js
+++ src/App.js
@@ -1,45 +1,65 @@
+import { useEffect, useState } from "react";
 import Home from "./components/home/Home";
 import SideNavbar from "./components/sidebar/SideNavbar";
+
 import './App.scss';
 
 function App() {
+  const [activeSectionId, setActiveSectionId] = useState("");
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setActiveSectionId(getActiveSectionId());
+      setSideBarSticky();
+    }
+  }, [window.scrollY])
+
+
+  function getActiveSectionId() {
+    let sectionId = "";
+    const sections = Array.from(document.getElementsByTagName("section"));
+      sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (window.scrollY >= sectionTop) {
+          sectionId = section.id;
+        }
+      })
+      return sectionId;
+  }
+
+  function setSideBarSticky() {
+    if (window.scrollY >= window.innerHeight) {
+      document.getElementById("sidebar").classList.add("sticky-top");
+    } else {
+      document.getElementById("sidebar").classList.remove("sticky-top");
+    }
+  }
+
   return (
     <>
       <div className="App container-fluid p-0">
-        <div className="row">
+        <div className="row vw-100 vh-100">
           <div className="col">
             <Home />
           </div>
         </div>
 
-        <div className="App container-fluid p-0">
-          <div className="row vw-100 vw-100">
-            <div className="col">
-              <SideNavbar />
-            </div>
-            <div className="col">
-              something
-            </div>
+        <div className="row content-container">
+          <div id="sidebar" className="col vh-100 vw-50">
+            <SideNavbar section={activeSectionId} />
           </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <SideNavbar />
-          </div>
-          <div className="col">
-            something
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <SideNavbar />
-          </div>
-          <div className="col">
-            something
-          </div>
-          <div className="col-sm">
-            <p>footer</p>
+          <div className="col overflow-auto">
+            <section id="about" className="vh-100">
+              <p> Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building software for an advertising agency, a start-up, a student-led design studio, and a huge corporation.</p>
+              <p>My main focus these days is building products and leading projects for our clients at Upstatement. In my free time I've also released an online video course that covers everything you need to know to build a web app with the Spotify API.</p>
+              <p>When I’m not at the computer, I’m usually rock climbing, hanging out with my wife and two cats, or running around Hyrule searching for Korok seeds</p>
+            </section>
+            <section id="experience" className="vh-100">
+              <p> experience</p>
+            </section>
+            <section id="projects" className="vh-100">
+              <p> project</p>
+            </section>
           </div>
         </div>
       </div>
--- src/components/sidebar/SideNavbar.js
+++ src/components/sidebar/SideNavbar.js
@@ -1,24 +1,66 @@
-export default function SideNavbar() {
+import { useEffect } from "react";
+import "./SideNavbar.scss";
+export default function SideNavbar({section}) {
+
+
+    useEffect(() => {
+      if (!section) {
+        return;
+      }
+
+      const elements = getLinks();
+      elements.forEach(item => {
+        item.classList.remove("navbar-link-active");
+      });
+
+      document.getElementById(section).classList.add("navbar-link-active");
+    },[section])
+
+    // setting the clicked link is the only active link
+    function handleNavItemClick(event){
+      const elements = getLinks();
+      elements.forEach(item => {
+        console.log(item)
+        item.classList.remove("navbar-link-active");
+      });
+
+      event.currentTarget.classList.add("navbar-link-active");
+    }
+
+    function getLinks() {
+      return Array.from(document.getElementsByClassName("navbar-link"));
+    }
 
   return (
     <>
-      <div className="short-description">
+      <div className="description">
         <h1 className="full-name">Trevor Tran</h1>
-        <h4 className="job-title">Job title</h4>
-        <p className="job-title">Job title</p>
-      </div>
+        <h4 className="job-title">Software Engineer</h4>
+        <p className="headline">I build reliable and scalable apps</p>
 
-      <div className="navigation d-flex flex-direction-column">
-        <div>
-          <a className="navbar-brand" href="#">Navbar</a>
-        </div>
-        <div>
-          <a className="navbar-brand" href="#">Navbar</a>
-        </div>
-        <div>
-          <a className="navbar-brand" href="#">Navbar</a>
-        </div>
-      </div>
+        <nav className="navbar hidden">
+          <ul>
+            <li>
+              <a id="about" className="navbar-link" href="#about" onClick={handleNavItemClick}>
+                <span className="navbar-link-indicator" />
+                <span>about</span>
+              </a>
+            </li>
+            <li>
+              <a id="experience" className="navbar-link" href="#experience" onClick={handleNavItemClick}>
+                <span className="navbar-link-indicator" />
+                <span>experience</span>
+              </a>
+            </li>
+            <li>
+              <a id="projects" className="navbar-link" href="#projects" onClick={handleNavItemClick}>
+                <span className="navbar-link-indicator" />
+                <span>projects</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div >
     </>
   )
 }
