7f6607b58239783eaf97578fc887df051a5c2e7e
Trevor Tran <fuongdtran@gmail.com>
2024-01-21 17:25:30 -0800

    add descriptive alt to homepage img

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ export default function Home() {
       </div>
       <div className="row">
         <div className="col text-center">
-        <img src={`${process.env.PUBLIC_URL}/diagram.gif`} className="img-fluid rounded mx-auto" alt="Portfolio deployment diagram"/>
+        <img src={`${process.env.PUBLIC_URL}/diagram.gif`} className="img-fluid rounded mx-auto" alt="Personal portfolio deployed on AWS infrastructure with scalability in mind"/>
         </div>
       </div>
 
