e7a40ee481d76874e709710ce44baf3ef260d41b
Trevor Tran <fuongdtran@gmail.com>
2024-02-13 00:34:43 -0800

    update projects descriptions

--- src/utils/aboutme.js
+++ src/utils/aboutme.js
@@ -2,7 +2,7 @@ export const persona = {
   name: "Trevor Tran",
   title: "Software Engineer",
   headline: "Motivated to learn and grow, ready to make an impact as a Software Engineer",
-  intro:[
+  intro: [
     `Driven by a lifelong passion for technology, I'm a Software Engineer honing my skills through the Per Scholas program and CS degree from University of Washington.
     Internship experience at Xemelgo and research projects have fueled my hands-on approach.
     Earning AWS certification reflects my continuous learning and commitment to building effective software.`,
@@ -20,9 +20,9 @@ export const experiences = [
       end: "Mar. 2020"
     },
     descriptions: [
-      "Administered the end-to-end functions of a new job tracking feature to aid customers in tracing actual labor hours.",
+      "Implemented and managed a new job tracking feature, streamlining customer access to real-time labor hour data.",
       "Successfully designed and deployed DynamoDB tables and RESTful APIs on AWS through utilization of Serverless framework.",
-      "Leveraged React Native to support mobile development and simulated on Android and iOS platforms."
+      "Conducted simulation and testing on Android and iOS platforms, resulting in a successful prototype demonstration to clients within a tight deadline."
     ],
     skills: ["JavaScript", "React", "HTML/CSS", "AWS", "Severless Framework"]
   },
@@ -34,8 +34,8 @@ export const experiences = [
       end: "Dec. 2019"
     },
     descriptions: [
-      "Improved the quality of a national curb dataset by assessing a sample of data points, identifying locations with incorrect curb data, and creating a data validation tool to improve the data accuracy in the future.",
-      "Established backend utilizing NodeJS, ExpressJS, Turf to process GeoJSON files from public city data."
+      "Conducted thorough analysis of data points in a national dataset to identify and rectify incorrect curb data in specific locations.",
+      "Utilized technical skills in NodeJS, ExpressJS, and Turf to create an automated data processing pipeline, eliminating the need for manual data extraction and saving 20 hours per week for the research team."
     ],
     skills: ["JavaScript", "React", "NodeJS"]
   },
@@ -64,9 +64,9 @@ export const projects = [
       end: "Feb. 2024"
     },
     descriptions: [
-      "Utilized technical skills in software development to innovate an interactive tool for seamless collaboration and demonstration within the team.",
-      "Empower teams to improved productivity and visualized creation in remote environment.",
-      "Designed and developed a robust and scalable web application by implementing industry best practices."
+      "Developed and implemented an interactive tool using software development skills to enhance team collaboration and demonstration.",
+      "Successfully facilitated virtual brainstorming sessions, enabling teams to effectively visualize and streamline the creative process.",
+      "Utilized technical skills in HTML, CSS, JavaScript, and ReactJS to optimize performance and enhance user experience."
     ],
     skills: ["JavaScript", "React", "HTML/CSS", "SCSS", "Bootstrap", "NodeJS", "MUI", "Socket.IO"]
   },
@@ -79,8 +79,9 @@ export const projects = [
       end: "Jan. 2024"
     },
     descriptions: [
-      "Develop personal responsive website to showcase my portfolio of software development projects.",
-      "Optimize website performance and usability with SEO best practices in mind.",
+      "Developed a responsive personal portfolio website to showcase software development projects, resulting in increased visibility and engagement.",
+      "Enhanced website performance and usability by implementing SEO best practices to achieve over 1400 daily requests.",
+      "Engineered and deployed a robust portfolio website on AWS EC2 instances with auto-scaling configuration behind an Elastic Load Balancer, achieving 99.99% uptime and enhancing security, thereby ensuring seamless user experience even during high-traffic periods."
     ],
     skills: ["JavaScript", "React", "HTML/CSS", "SCSS", "Bootstrap", "MUI"]
   },
@@ -95,6 +96,6 @@ export const projects = [
       "Extracted stock data from Quandl financial services API and inserted into MySQL database.",
       "Adopted Object-oriented design and MVC, organizing the code for maintainability and scalability."
     ],
-    skills: ["Java", "JavaScript", "React", "HTML/CSS", "SQL"]
+    skills: ["Java", "JavaScript", "React", "HTML/CSS", "MySQL"]
   }
 ]
